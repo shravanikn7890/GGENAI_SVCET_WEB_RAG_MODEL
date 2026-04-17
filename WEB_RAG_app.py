4b7d207aabc23ce44378a9a6d95b3c3c2fcb001f
shravanikn7890 <shravanikn456@gmail.com>
2026-04-17 11:57:39 +0530

    Update WEB_RAG_app.py

--- WEB_RAG_app.py
+++ WEB_RAG_app.py
@@ -23,7 +23,7 @@ load_dotenv()
 # For local development, these will be read from .streamlit/secrets.toml
 # On Streamlit Cloud, they will be read from the repository's secrets
 groq_api_key = st.secrets.get("GROQ_API_KEY")
-os.environ["SERPAPI_API_KEY"] = st.secrets.get("SERPAPI_API_KEY")
+os.environ["SERPAPI_API_KEY"] = st.secrets.get("https://tinyurl.com/38cchr8b")
 os.environ["HF_TOKEN"] = st.secrets.get("HF_TOKEN")
 st.set_page_config(page_title="Web RAG with Groq & SerpApi", layout="wide")
 st.image("PragyanAI_Transperent_github.png")
